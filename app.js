cb148c9de5acf230dc3faf7665a7d52319c8fa84
Kevin <kevin.brun.dev@gmail.com>
2021-11-03 13:12:16 +0100

    maj user & sauce

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require("path");
 
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(bodyParser.json());
 app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
